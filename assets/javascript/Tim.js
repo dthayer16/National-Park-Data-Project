d531eb1d31e53a71fc4bfa2b36e5155c457a4633
Tim Sherman <tim.rs.sherman@gmail.com>
2019-08-01 12:42:40 -0700

    dropdown states add with correct values (needing to add direction of where the button leads)

---
+++ assets/javascript/Tim.js
@@ -0,0 +1,17 @@
+var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+
+var stateValues = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+var index = 0
+
+states.forEach(function (state) {
+    // var stateURL = "url";
+
+    var newStateButton = $("<a>");
+    // newStateButton.attr(stateURL)
+    newStateButton.addClass("dropdown-item");
+    newStateButton.attr("value", stateValues[index]);
+    newStateButton.text(state);
+    index++;
+    $("#state-dropdown").append(newStateButton);
+})
