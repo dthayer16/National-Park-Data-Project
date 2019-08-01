14f362976f5dbc06621da754e5e439346122dc9e
Tim Sherman <tim.rs.sherman@gmail.com>
2019-08-01 13:59:48 -0700

    attr for dropdowns complete, needing to add to ajax calls

--- assets/javascript/Tim.js
+++ assets/javascript/Tim.js
@@ -1,17 +1,73 @@
-var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+var states = [
+    {name: "Alabama", abbr: "AL"},
+    {name: "Alaska", abbr: "AK"},
+    {name: "Arizona", abbr: "AZ"},
+    {name: "Arkansas", abbr: "AR"},
+    {name: "California", abbr: "CA"},
+    {name: "Colorado", abbr: "CO"},
+    {name: "Connecticut", abbr: "CT"},
+    {name: "Delaware", abbr: "DE"},
+    {name: "Florida", abbr: "FL"},
+    {name: "Georgia", abbr: "GA"},
+    {name: "Hawaii", abbr: "HI"},
+    {name: "Idaho", abbr: "ID"},
+    {name: "Illinois", abbr: "IL"},
+    {name: "Indiana", abbr: "IN"},
+    {name: "Iowa", abbr: "IA"},
+    {name: "Kansas", abbr: "KS"},
+    {name: "Kentucky", abbr: "KY"},
+    {name: "Louisiana", abbr: "LA"},
+    {name: "Maine", abbr: "ME"},
+    {name: "Maryland", abbr: "MD"},
+    {name: "Massachusetts", abbr: "MA"},
+    {name: "Michigan", abbr: "MI"},
+    {name: "Minnesota", abbr: "MN"},
+    {name: "Mississippi", abbr: "MS"},
+    {name: "Missouri", abbr: "MO"},
+    {name: "Montana", abbr: "MT"},
+    {name: "Nebraska", abbr: "NE"},
+    {name: "Nevada", abbr: "NV"},
+    {name: "New Hampshire", abbr: "NH"},
+    {name: "New Jersey", abbr: "NJ"},
+    {name: "New Mexico", abbr: "NM"},
+    {name: "New York", abbr: "NY"},
+    {name: "North Carolina", abbr: "NC"},
+    {name: "North Dakota", abbr: "ND"},
+    {name: "Ohio", abbr: "OH"},
+    {name: "Oklahoma", abbr: "OK"},
+    {name: "Oregon", abbr: "OR"},
+    {name: "Pennsylvania", abbr: "PA"},
+    {name: "Rhode Island", abbr: "RI"},
+    {name: "South Carolina", abbr: "SC"},
+    {name: "South Dakota", abbr: "SD"},
+    {name: "Tennessee", abbr: "TN"},
+    {name: "Texas", abbr: "TX"},
+    {name: "Utah", abbr: "UT"},
+    {name: "Vermont", abbr: "VT"},
+    {name: "Virginia", abbr: "VA"},
+    {name: "Washington", abbr: "WA"},
+    {name: "West Virginia", abbr: "WV"},
+    {name: "Wisconsin", abbr: "WI"},
+    {name: "Wyoming", abbr: "WY"}
+];
 
-var stateValues = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-
-var index = 0
+var index = 0;
 
 states.forEach(function (state) {
-    // var stateURL = "url";
-
     var newStateButton = $("<a>");
-    // newStateButton.attr(stateURL)
+    
     newStateButton.addClass("dropdown-item");
-    newStateButton.attr("value", stateValues[index]);
-    newStateButton.text(state);
+    newStateButton.attr("value", state.abbr);
+    newStateButton.text(state.name);
     index++;
     $("#state-dropdown").append(newStateButton);
+});
+
+states.forEach(function (state) {
+    var stateOption = $("<option>");
+
+    stateOption.attr("value", state.abbr);
+    stateOption.text(state.name);
+    index++;
+    $("#find-state-parks").append(stateOption);
 })
